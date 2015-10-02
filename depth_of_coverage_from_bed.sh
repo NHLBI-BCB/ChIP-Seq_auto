66984198e22c8d5d5a7cb28816df90ec808b0d6f
vd_ngs <vd4mmind@gmail.com>
2015-10-02 12:28:50 +0200

    Functionality
    This code really gives the real sequencing depth for each independent region defined in the bed file. The for loop reads each line, transforms it into a chr:start-end format, calls samtools depth to get the coverage of each base of each individual region, and the awk section does the calculation for each individual region. hence, the output of the script is a line per bed file region, followed by its average and maximum coverages. if there are any regions that overlap it won't affect the coverage calculation of each individual region.

--- depth_of_coverage_from_bed.sh
+++ depth_of_coverage_from_bed.sh
@@ -1,3 +1,5 @@
+
+#tring to retrieve coverage information for each bed file region
 for region in `cat regions.bed | sed 's/\t/:/' | sed 's/\t/-/'`; do
   echo -n "$region "
   samtools depth -r $region reads.bam | awk '{c++;s+=$3;if($3>M)M=$3}END{print s/c, M}'
